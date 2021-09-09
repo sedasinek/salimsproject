830f8d7edaf86121154fca067541f3568d17a5a4
“sedasinek” <sedaasnek@gmail.com>
2021-09-09 15:59:48 +0300

    deneme

--- src/main/java/com/yelloware/Application.java
+++ src/main/java/com/yelloware/Application.java
@@ -8,9 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Application {
 
     public static void main(String[] args) {
-    	BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
-        String pwd = bcryptPasswordEncoder.encode("password");
-        System.out.println(pwd);
         SpringApplication.run(Application.class, args);
     }
 }
